4c21b9b33044e594ea6ae17ca12fa27452b4473b
acejilam <acejilam@gmail.com>
2025-08-20 17:57:32 +0800

    -
    Signed-off-by: acejilam <acejilam@gmail.com>

--- sub/config.py
+++ sub/config.py
@@ -1,6 +1,6 @@
 import os.path
 
-config_path = '/Users/acejilam/data/wechat'
+config_path = '/Users/acejilam/Documents/SyncZone/wechat'
 
 wechat_conf = os.path.join(config_path, "wechat.json")
 mysql_conf = os.path.join(config_path, "mysql.json")
